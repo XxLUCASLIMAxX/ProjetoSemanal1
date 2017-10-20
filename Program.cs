e9bc148cbc5aa9ba1af4569d4cf7eb043ff8dfd1
lucas Lima <lucas-limagomes@hotmail.com>
2017-10-20 21:51:01 -0200

    Validação CPF feita/retorna true or false

--- Program.cs
+++ Program.cs
@@ -5,106 +5,45 @@ namespace CalculoCPF
 {
     class Program
     {
-        static Boolean checkCpf(int[]v){
-            Boolean retorno= false;
-            int[] n=new int[2];
-            int soma=0 ;
-            int mult=10;
-            int resto=0;
-            n[0]=v[9];
-            
-              
-            n[1]=v[10];
-             
-            for(int i=0;i<9;i++){
-                soma+=v[i]*mult;
-               
-                
-                 mult--;
-            }
-            resto=soma%11;
 
-            
-           
-         
 
-            if(resto<2){
-            
-                resto=0;
 
-                if(resto==n[0]){
-                    
-              
-                    mult=11;
-                    soma=0;
-                    
-                    for(int i =0;i<10;i++){
-                        soma+=v[i]*mult;
-                        mult--;
-                    } 
-                    resto=soma%11;
-                    
-                    if (resto<2){
-                      
-             
-                        resto=0;
-                        if(resto==n[1]){
-                              Console.WriteLine("CPF VALIDO");
-                            retorno=true;
-                        }else{
-                            Console.WriteLine("cpf invalido");
-                        }
-                    }else{
-                        resto=11-resto;
-                         
-                        if(resto==n[1]){
-                              Console.WriteLine("CPF VALIDO");
-                            retorno=true;
-                        }else{
-                            Console.WriteLine("cpf invalido");
-                        }
-                    }
 
-                }
 
-            }else{
-                resto=11-resto;
-                
-                
-                if(resto==n[0]){
-                     
-                     mult=11;
-                    soma=0;
-                    
-                    for(int i =0;i<10;i++){
-                        soma+=v[i]*mult;
-                        mult--;
-                    } 
-                    resto=soma%11;
-                    if (resto<2){
-                        resto=0;
-                        if(resto==n[1]){
-                            Console.WriteLine("CPF VALIDO");
-                            retorno=true;
-                        }else{
-                            Console.WriteLine("cpf invalido");
-                        }
-                    }else{
-                        resto=11-resto;
-                        if(resto==n[1]){
-                              Console.WriteLine("CPF VALIDO");
-                            retorno=true;
-                        }else{
-                            Console.WriteLine("cpf invalido");
-                        }
-                   
-                }
-            }
-       }
+     
+        static Boolean checkCpf(string cpf){
+            string tempCpf;
+		   string digito;
+		   int soma;
+		   int resto;
 
-            return retorno;
+            int[] mult1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+		    int[] mult2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            	   
+		tempCpf = cpf.Substring(0, 9);
+		soma = 0;
+        for(int i=0; i<9; i++){
+		    soma += int.Parse(tempCpf[i].ToString()) * mult1[i];
         }
-        static void preencher(string cpf,int[] numeros){
+		resto = soma % 11;
+		if ( resto < 2 ){
+             resto = 0;
+        }else
+		   resto = 11 - resto;
+		digito = resto.ToString();
+		tempCpf = tempCpf + digito;
+		soma = 0;
+		for(int i=0; i<10; i++)
+		    soma += int.Parse(tempCpf[i].ToString()) * mult2[i];
+		resto = soma % 11;
+		if (resto < 2)
+		   resto = 0;
+		else
+		   resto = 11 - resto;
+		digito = digito + resto.ToString();
+		return cpf.EndsWith(digito);
+        }
+        static void preencher(string cpf){
             int aux=0;
              while(aux==0){
 
@@ -133,14 +72,11 @@ namespace CalculoCPF
             Console.Write("Digite o cpf: ");
             cpf=Console.ReadLine();
 
-            preencher(cpf,numeros);
-            checkCpf(numeros);
+            preencher(cpf);
+            checkCpf(cpf);
+            Console.WriteLine(checkCpf(cpf));
 
-            if(checkCpf(numeros)==false){
-                Console.WriteLine("cpf falso");
-                
-            }
-           
+            
 
 
       
